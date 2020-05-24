306e8284388994f5edf1f87fad19362cbeb31367
Moksiy <maksaligin@gmail.com>
2020-05-25 00:13:54 +0300

    IsEnabled button AuthorizationPage

--- Pavilions_WS/ViewModels/AuthorizationViewModel.cs
+++ Pavilions_WS/ViewModels/AuthorizationViewModel.cs
@@ -7,11 +7,54 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using Pavilions_WS.Logic;
 using System.ComponentModel.DataAnnotations;
+using System.Windows;
 
 namespace Pavilions_WS.ViewModels
 {
-    class AuthorizationViewModel : BaseViewModel, IPageViewModel
+    class AuthorizationViewModel : ValidateViewModel, IPageViewModel
     {
-        
+        /*private string login;
+
+        public string Login
+        {
+            get => login;
+            set => login = value;
+        }
+
+        private string password;
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }*/
+
+
+        string login;
+        [Required]
+        public string Login
+        {
+            get => login;
+            set => Set(ref login, value);            
+        }
+
+        string password;
+        [Required]
+        public string Password
+        {
+            get => password;
+            set => Set(ref password, value);
+        }
+
+        private ICommand testC;
+        public ICommand TestC
+        {
+            get
+            {
+                return testC ?? (testC = new RelayCommand(x =>
+                {
+                    MessageBox.Show(login + "\n" + password);                    
+                }));
+            }
+        }
     }
 }
---
+++ Pavilions_WS/ViewModels/ValidateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavilions_WS.ViewModels
+{
+    abstract class ValidateViewModel : BaseValidateModel
+    {
+        protected override bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            var b = base.Set(ref field, value, propertyName);
+            if (b && GetType().GetProperty(propertyName).GetCustomAttributes(typeof(RequiredAttribute), true).Length > 0)
+            {
+                IsValid = GetType().GetProperties()
+                                   .Where(p => p.GetCustomAttributes(typeof(RequiredAttribute), true).Length > 0)
+                                   .Select(p => (string)p.GetValue(this))
+                                   .All(s => !string.IsNullOrEmpty(s));
+                NotifyPropertyChanged(nameof(IsValid));
+            }
+            return b;
+        }
+
+        public bool IsValid { get; private set; }
+    }
+}
+
